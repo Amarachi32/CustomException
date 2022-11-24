0e69a7e4d9e7f0fa6d674cffe91baeab46a659e8
IZUEGBUNAM PERPETUA AMARACHI <54138835+Amarachi32@users.noreply.github.com>
2022-11-24 09:44:32 +0100

    Add files via upload

---
+++ CustomException/CustomException/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomException
+{
+    public class CustomException : Exception
+    {
+        public CustomException()
+        {
+        }
+        public CustomException(string message) : base(message)
+        {
+        }
+        public CustomException(string message, Exception innerexception) : base(message, innerexception)
+        {
+        }
+    }
+}
---
+++ CustomException/CustomException/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace CustomException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your  name");
+            string name = Console.ReadLine();
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new CustomException(" input your Name");
+            }
+            else
+            {
+                Console.WriteLine("Name: " + name);
+            }
+            Console.ReadLine();
+             
+        }
+    }
+    
+}
